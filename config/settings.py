8a0e7381de57d05d78cc2816f8573fa7ebca8aa6
mariyam-maqsood <itsmariamahmad@gmail.com>
2026-04-03 15:06:44 +0500

    Added secret keys to env

--- config/settings.py
+++ config/settings.py
@@ -72,7 +72,7 @@ EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = 'REMOVED'
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'itsmariamahmad@gmail.com'
 
 
